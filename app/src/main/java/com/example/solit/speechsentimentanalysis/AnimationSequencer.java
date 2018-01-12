ad07eacc723a3f01bd3861e59befbb280afe5b67
Solitonjet <solitonjet@gmail.com>
2018-01-13 03:01:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/solit/speechsentimentanalysis/AnimationSequencer.java
@@ -0,0 +1,169 @@
+package com.example.solit.speechsentimentanalysis;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by solit on 2018/01/12.
+ */
+
+public class AnimationSequencer<T> {
+
+    private final AnimationSequencerCallback mCallback;
+    public static abstract class AnimationSequencerCallback {
+        public void onAnimationEnd() {}
+
+    }
+
+    public class AnimationSequenceParams {
+        int type;
+        float[] values;
+        int time;
+    }
+
+    public static final int ANIM_ALPHA      = 100;
+    public static final int ANIM_ROTATION   = 101;
+    public static final int ANIM_SCALE      = 102;
+    public static final int ANIM_TRANSLATE  = 103;
+
+    private ObjectAnimator mObjectAnimator;
+
+    private T mView;
+    public AnimationSequencer(T _view, AnimationSequencerCallback _callback) {
+        mView = _view;
+        mCallback = _callback;
+        mObjectAnimator = new ObjectAnimator();
+    }
+
+    public void doAnimate (List<AnimationSequenceParams> sequence) {
+        setSequenseList(sequence);
+
+        mAnimSequenceHandler.post(mAnimSequencer);
+    }
+
+    private Handler mAnimSequenceHandler = new Handler();
+    private Runnable mAnimSequencer = new Runnable() {
+        @Override
+        public void run() {
+            if (null == getNextSequenceParam()) {
+                // finished
+                mCallback.onAnimationEnd();
+                return;
+            }
+
+            float[] values = getNextSequenceParam().values;
+            int time = getNextSequenceParam().time;
+
+            switch( getNextSequenceParam().type) {
+                case ANIM_ALPHA:
+                    animateAlpha(mView, values, time);
+                    break;
+                case ANIM_ROTATION:
+                    animateRotation(mView, values, time);
+                    break;
+                case ANIM_SCALE:
+                    animateScale(mView, values, time);
+                    break;
+                case ANIM_TRANSLATE:
+                    animateTranslation(mView, values, time);
+                    break;
+            }
+        }
+    };
+
+    List<AnimationSequenceParams> mSequenseList;
+    private void setSequenseList(List<AnimationSequenceParams> sequenseList) {
+        mSequenseList = sequenseList;
+    }
+    private List<AnimationSequenceParams> getSequenseList() {
+        return mSequenseList;
+    }
+
+    private int iSeqId;
+    private int getSequenceId() {
+        return iSeqId;
+    }
+    private void inclementSequenceId() {
+        iSeqId++;
+    }
+
+    private AnimationSequenceParams getNextSequenceParam() {
+        if (getSequenseList().size() > getSequenceId()) {
+            return getSequenseList().get(getSequenceId());
+        }
+
+        return null;
+    }
+
+    private AnimatorListenerAdapter mAnimAdapter = new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            Log.d("TEST", "onAnimationEnd()");
+            inclementSequenceId();
+            mAnimSequenceHandler.post(mAnimSequencer);
+        }
+    };
+
+    private void animateAlpha( T target, float[] values, int time ) {
+
+        mObjectAnimator = ObjectAnimator.ofFloat( target, "alpha", 0f, 1f );
+        mObjectAnimator.setDuration( time );
+        mObjectAnimator.addListener(mAnimAdapter);
+
+        mObjectAnimator.start();
+    }
+
+    private void animateRotation( T target, float[] values, int time ) {
+
+        float start = values[0];
+        float end = values[1];
+
+        mObjectAnimator = ObjectAnimator.ofFloat( target, "rotation", start, end );
+        mObjectAnimator.setDuration( time );
+        mObjectAnimator.addListener(mAnimAdapter);
+
+        mObjectAnimator.start();
+    }
+
+    private void animateScale( T target, float[] values, int time ) {
+
+        float fromX = values[0];
+        float toX = values[1];
+        float fromY = values[2];
+        float toY = values[3];
+
+        PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat( "scaleX", fromX, toX );
+        PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat( "scaleY", fromY, toY );
+
+        mObjectAnimator = ObjectAnimator.ofPropertyValuesHolder( target, holderX, holderY );
+        mObjectAnimator.setDuration( time );
+        mObjectAnimator.addListener(mAnimAdapter);
+
+        mObjectAnimator.start();
+
+    }
+
+    private void animateTranslation( T target, float[] values, int time ) {
+
+        float fromX = values[0];
+        float toX = values[1];
+        float fromY = values[2];
+        float toY = values[3];
+
+        PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat( "translationX", fromX, toX );
+        PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat( "translationY", fromY, toY );
+
+        mObjectAnimator = ObjectAnimator.ofPropertyValuesHolder( target, holderX, holderY );
+        mObjectAnimator.setDuration( time );
+        mObjectAnimator.addListener(mAnimAdapter);
+
+        mObjectAnimator.start();
+
+    }
+}
---
+++ app/src/main/java/com/example/solit/speechsentimentanalysis/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.solit.speechsentimentanalysis;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.solit.speechsentimentanalysis.AnimationSequencer.ANIM_ALPHA;
+import static com.example.solit.speechsentimentanalysis.AnimationSequencer.ANIM_ROTATION;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final AnimationSequencer.AnimationSequencerCallback mAnimCallback
+                            = new AnimationSequencer.AnimationSequencerCallback() {
+        @Override
+        public void onAnimationEnd() {
+            super.onAnimationEnd();
+
+            Log.d("TEST", "Animation Ended");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_start = (Button)findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btn_move = (Button)findViewById(R.id.btn_entity);
+                AnimationSequencer sequencer = new AnimationSequencer<Button>(btn_move, mAnimCallback);
+                ArrayList<AnimationSequencer.AnimationSequenceParams> paramlist = new ArrayList<>();
+
+                AnimationSequencer.AnimationSequenceParams params;
+
+                params = sequencer.new AnimationSequenceParams();
+                params.type = ANIM_ALPHA;
+                params.values = new float[]{0f, 1f};
+                params.time = 1000;
+                paramlist.add(params);
+
+                params = sequencer.new AnimationSequenceParams();
+                params.type = ANIM_ROTATION;
+                params.values = new float[]{0f, 360f};
+                params.time = 3000;
+                paramlist.add(params);
+
+                sequencer.doAnimate(paramlist);
+            }
+        });
+    }
+
+
+}
